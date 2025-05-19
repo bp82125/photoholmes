ee64af914cade4ba30bbcb51856cce88540081c8
nhat82125 <anios82125@gmail.com>
2025-05-19 09:07:51 +0700

    remove the unused module in mesorch

--- src/photoholmes/methods/mesorch/method.py
+++ src/photoholmes/methods/mesorch/method.py
@@ -16,21 +16,6 @@ from photoholmes.methods.base import BaseTorchMethod, BenchmarkOutput
 
 sys.path.append('.')
 
-
-class DWConv(nn.Module):
-    def __init__(self, dim=768):
-        super(DWConv, self).__init__()
-        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
-
-    def forward(self, x, H, W):
-        B, N, C = x.shape
-        x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.dwconv(x)
-        x = x.flatten(2).transpose(1, 2)
-
-        return x
-
-
 class UpsampleConcatConv(nn.Module):
     def __init__(self):
         super(UpsampleConcatConv, self).__init__()
